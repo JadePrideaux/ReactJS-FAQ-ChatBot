96d6fdb6b8a8e033ad5c7200f579ec147abf2621
jadeprideaux <jadeprideaux7@gmail.com>
2025-10-01 12:54:58 +0100

    feat: added script to download dataset from kaggle

---
+++ backend/logic/downloaddataset.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+import kagglehub
+
+# Download to KaggleHub cache
+path = kagglehub.dataset_download("savanidhruv/reactjs-faq-dataset")
+
+# File inside the dataset
+src = os.path.join(path, "React_dataset.json")
+
+# Relative path to dataset
+dataset = "backend/data/React_dataset.json"
+
+# Make sure the folder exists
+os.makedirs(os.path.dirname(dataset), exist_ok=True)
+
+# Copy the file over
+shutil.copy(src, dataset)
+
+print(f"Copied dataset to {dataset}")
--- backend/logic/getanswers.py
+++ backend/logic/getanswers.py
@@ -1,3 +1,8 @@
-# placeholder function - need to get the answer here later
+import pandas as pd
+
+# Get the dataset
+df = pd.read_json("backend/data/React_dataset.json")
+print(df)
+
 def get_answer(question: str) -> str:
   return f"You asked: {question}. Placeholder answer."
